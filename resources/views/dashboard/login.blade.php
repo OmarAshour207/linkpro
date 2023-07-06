94250cdcbfab8ba63da739bc3d8e97aa216e9c8f
Omar Ashour <omarashour207@gmail.com>
2023-07-06 22:01:03 +0200

    favicon

--- resources/views/dashboard/login.blade.php
+++ resources/views/dashboard/login.blade.php
@@ -5,6 +5,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="icon" type="image/x-icon" href="{{ asset('dashboard/images/logos/logo.jpg') }}">
     <title>{{ __('Login') }}</title>
 
     <!-- Prevent the demo from appearing in search engines -->
